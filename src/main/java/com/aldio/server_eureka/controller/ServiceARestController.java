4e1fa27b38156110a342be64780f2585bbeae6d0
aldio <muhamadaldioyaspindo@gmail.com>
2025-09-09 19:16:39 +0700

    first commit

---
+++ src/main/java/com/aldio/server_eureka/controller/ServiceARestController.java
@@ -0,0 +1,14 @@
+package com.aldio.server_eureka.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceARestController {
+
+	@GetMapping("/helloWorld")
+	public String helloWorld() {
+		return "Hello world from Service A!";
+	}
+
+}
